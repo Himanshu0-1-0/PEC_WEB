c4bccf92afd95c5f002b0fd2efca85f900932656
Himanshu <himanshumehta8104@gmail.com>
2024-07-11 19:30:01 +0530

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./App.css"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Components/Signin/Signin';
 import ProtectedRoute from './Components/Protection/Protection';
